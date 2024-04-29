65cfb86024d4426117c086f1c383e16fd82428ba
smierl <145398729+smierl@users.noreply.github.com>
2024-04-29 12:29:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ loginBtn.addEventListener('click', () =>
 {
     container.classList.remove('active');
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const btnAceptarPrivacidad = document.getElementById('btn-aceptar-privacidad');
+    const avisoPrivacidad = document.getElementById('aviso-privacidad');
+    const fondoAviso = document.getElementById('fondo-aviso');
+
+    avisoPrivacidad.classList.add('activo');
+    fondoAviso.classList.add('activo');
+
+    btnAceptarPrivacidad.addEventListener('click', () => {
+        avisoPrivacidad.classList.remove('activo');
+        fondoAviso.classList.remove('activo');
+
+        localStorage.setItem('terminos-aceptados', true);
+    });
+});
